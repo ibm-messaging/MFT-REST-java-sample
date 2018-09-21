a741680ae74f7a17623e67d92efce4640281d016
Gandhimathy Srinivasan <gsriniva@in.ibm.com>
2018-09-21 13:46:51 +0530

    Update AgentStatus.java
    done formatting.

--- mftRestSample/AgentStatus.java
+++ mftRestSample/AgentStatus.java
@@ -1,3 +1,9 @@
+/**
+ * © Copyright IBM Corporation 2018, 2020
+ * author: Gandhimathy Srinivasan 
+ * mail: gsriniva@in.ibm.com, gandhi.srini@gmail.com
+ */
+
 package mftRestSample;
 
 import java.io.IOException;
@@ -10,100 +16,89 @@ import org.json.JSONException;
 
 public class AgentStatus extends AbstractQuery{
 	
-	private static final List<String> attributesRequired = new ArrayList<>();
-	private static final List<String> allAttributes = new ArrayList<>();
+     private static final List<String> attributesRequired = new ArrayList<>();
+     private static final List<String> allAttributes = new ArrayList<>();
 	
-	public AgentStatus()
-	 {
+     public AgentStatus() {
 		 super();
 	 }
 	
-	 public AgentStatus(String hostName, String portNumber)
-	 {
+    public AgentStatus(String hostName, String portNumber) {
 		 super(hostName,portNumber);
 	 }
 	
-	 public AgentStatus(String hostName, String portNumber,Boolean attributeRequired,String queryAttribute)
-	 {
+    public AgentStatus(String hostName, String portNumber,Boolean attributeRequired,String queryAttribute) {
 		 super(hostName,portNumber,true,queryAttribute);
 	 }
 	 
-	 @Override
-	 protected List<String> getBasicAttributeList() 
-	 {
-		 
+    @Override
+    protected List<String> getBasicAttributeList() {
 		return attributesRequired;	 	
 	  }
 	 
-	 private static void printHelp()
-	 {
-		 System.out.println("AgentStatus <HostName/HostIP> <PortNumber> <queryAttribute> <attributesToQuery>");
-		 System.out.println("================================================================");
-		 System.out.println("Where -");
-		 System.out.println("");
-		 System.out.println("HostName: IP address or the Host name of the server where REST service is running.");
-		 System.out.println("          It is an optional parameter, but it should be provided with PortNumber");
-		 System.out.println("");                
-		 System.out.println("PortNumber: The port, at which the REST service is hosted in the Server.");
-		 System.out.println("          It is an optional parameter, but it should be provided with HostName");
-		 System.out.println("");
-		 System.out.println("queryAttribute: Valid values are true/false. ");
-		 System.out.println("                It is optional, but it should be provided with attributesToQuery");
-		 System.out.println("                should be provided as next argument");
-		 System.out.println("");
-		 System.out.println("attributsToQuery: Valid values are all/default ");
-		 System.out.println("                It is optional, but it should be provided with queryAttribute");
-		 System.out.println("================================================================");
-		 System.out.println("Examples -");
-		 System.out.println("");
-		 System.out.println("1.) To query the default attributes of agents on a local system, which is configured to port 9080");
-		 System.out.println("AgentStatus");
-		 System.out.println("");
-		 System.out.println("2.) To query the default attributes of agents on a different server");
-		 System.out.println("AgentStatus 9.30.10.74 5050" );
-		 System.out.println("");
-		 System.out.println("3.) To query the all attributes of agents on a different server");
-		 System.out.println("AgentStatus 9.30.10.74 5050 true all" );
-		 System.out.println("");
-		 System.out.println("================================================================");
-	 }
+   private static void printHelp() {
+         System.out.println("AgentStatus <HostName/HostIP> <PortNumber> <queryAttribute> <attributesToQuery>");
+	 System.out.println("================================================================");
+	 System.out.println("Where -");
+	 System.out.println("");
+	 System.out.println("HostName: IP address or the Host name of the server where REST service is running.");
+	 System.out.println("          It is an optional parameter, but it should be provided with PortNumber");
+	 System.out.println("");                
+	 System.out.println("PortNumber: The port, at which the REST service is hosted in the Server.");
+	 System.out.println("          It is an optional parameter, but it should be provided with HostName");
+	 System.out.println("");
+	 System.out.println("queryAttribute: Valid values are true/false. ");
+	 System.out.println("                It is optional, but it should be provided with attributesToQuery");
+	 System.out.println("                should be provided as next argument");
+	 System.out.println("");
+	 System.out.println("attributsToQuery: Valid values are all/default ");
+	 System.out.println("                It is optional, but it should be provided with queryAttribute");
+	 System.out.println("================================================================");
+	 System.out.println("Examples -");
+	 System.out.println("");
+	 System.out.println("1.) To query the default attributes of agents on a local system, which is configured to port 9080");
+	 System.out.println("AgentStatus");
+	 System.out.println("");
+	 System.out.println("2.) To query the default attributes of agents on a different server");
+	 System.out.println("AgentStatus 9.30.10.74 5050" );
+	 System.out.println("");
+	 System.out.println("3.) To query the all attributes of agents on a different server");
+	 System.out.println("AgentStatus 9.30.10.74 5050 true all" );
+	 System.out.println("");
+	 System.out.println("================================================================");
+ }
 	
-	 public static void main(String[] args) throws MalformedURLException, IOException, JSONException
-	 {
-		 attributesRequired.add("name");
-  	     attributesRequired.add("state");
-  	     attributesRequired.add("type");
+ public static void main(String[] args) throws MalformedURLException, IOException, JSONException
+ {
+	 attributesRequired.add("name");
+         attributesRequired.add("state");
+         attributesRequired.add("type");
   	     
-  	   
-	   allAttributes.add("general");
-	   allAttributes.add("qmgrConnection");
-	   allAttributes.add("connectDirectBridge");
-	   allAttributes.add("protocolBridge");
+	 allAttributes.add("general");
+	 allAttributes.add("qmgrConnection");
+	 allAttributes.add("connectDirectBridge");
+	 allAttributes.add("protocolBridge");
 	   
-		
-  	     switch (args.length) {
-  	     case 0:
-  	    	new AgentStatus().getStatus();
-  	    	break;
-		 case 2: 
-		    new AgentStatus(args[0],args[1]).getStatus();
-			break; 
-		 case 4: 
-			if( (args[2].equals("true")) && (args[3].equals("all") ) )
-			{
-		      
-			   attributesRequired.addAll(allAttributes);
-			   
-	 		   new AgentStatus(args[0], args[1],true,"*").getStatus();  
-			}
-			else if ( (args[2] == "false") || (args[2] == "default") )
-			    new AgentStatus(args[0], args[1]).getStatus();
+	switch (args.length) {
+  	case 0:
+  	    new AgentStatus().getStatus();
+  	    break;
+        case 2: 
+            new AgentStatus(args[0],args[1]).getStatus();
+	    break; 
+        case 4: 
+	    if( (args[2].equals("true")) && (args[3].equals("all") ) ) {
+                attributesRequired.addAll(allAttributes);
+	        new AgentStatus(args[0], args[1],true,"*").getStatus();  
+	     }
+	    else if ( (args[2] == "false") || (args[2] == "default") )
+		new AgentStatus(args[0], args[1]).getStatus();
             else
-			  printHelp();
-			break; 
-		default: 
-		  printHelp();
-		  break; 
-		} //end of switch-case
-	}//end of main
+		printHelp();
+		break; 
+        default: 
+	  printHelp();
+	  break; 
+	} //end of switch-case
+   }//end of main
 } //end of Class
